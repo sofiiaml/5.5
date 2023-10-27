3b4e1c05e4d36f42c2e7fbae9b1bb1f466433754
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-10-27 12:40:45 +0300

    lab5.5

---
+++ 555/555.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std; 
+double bn(int b1, int q, int n)
+{
+    if (n == 1)
+        return b1;
+    else
+    return q * bn(b1, q, n - 1);
+}
+
+double S(int b1, int q, int n, int &depth, int level) 
+{if (level > depth)
+            depth = level; 
+cout << "level=" << level << endl;
+    if (n == 1) {
+        return b1;
+    }
+    else {
+        // cout << "S depth=" << depth << endl;
+        double t = bn(b1, q, n);
+        return t + S(b1, q, n - 1,depth, level+1);
+    }
+}
+
+int main()
+{
+    int b1, q, n;
+    cout << "b1= "; cin >> b1;
+    cout << "q= "; cin >> q;
+    cout << "n= "; cin >> n;  
+    int depth = 0;
+    double t = bn(b1, q, n);
+    cout << "bn=" << t << endl;
+   
+   
+    double sum = S(b1, q, n, depth,1);
+    cout << "S= " << sum << endl;
+ cout << "depth=" << depth << endl;
+  
+    return 0;
+}
---
+++ unittest5.5/unittest5.5.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../555/555.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace unittest55
+{
+	TEST_CLASS(unittest55)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = bn(2, 4, 3);
+			Assert::AreEqual(t, 32, 0.000001);
+
+		}
+	};
+}
